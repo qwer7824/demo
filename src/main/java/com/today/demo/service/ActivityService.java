9b4de0396b88c904ba52969a83962a8994423a50
qwer7824 <sh7824@naver.com>
2023-10-31 14:41:49 +0900

    활동 추가 로직 변경
    venue,capacity 0(전체) 으로 설정시 실내 ,실외 , 인원수 4인용까지 한번에 들어가게 구현

--- src/main/java/com/today/demo/service/ActivityService.java
+++ src/main/java/com/today/demo/service/ActivityService.java
@@ -15,45 +15,45 @@ import java.util.Random;
 @Slf4j
 public class ActivityService {
 
-        private final ActivityRepository activityRepository;
-        private final ActivityRedisTemplateService activityRedisTemplateService;
+    private final ActivityRepository activityRepository;
+    private final ActivityRedisTemplateService activityRedisTemplateService;
 
-        public String searchRandomActivityByVenueAndCapacity(int venue, int capacity) {
+    public String searchRandomActivityByVenueAndCapacity(int venue, int capacity) {
 
-            List<Activity> activities;
+        List<Activity> activities;
 
-            if (venue == 0 && capacity == 0) {
-                activities = activityRedisTemplateService.findAll(); // Redis에서 모든 활동 객체 목록을 가져옴
-                if(activities.isEmpty()){ // 만약 레디스에 없으면 db호출
-                    activities = activityRepository.findAll();
-                }
-            } else if (venue == 0) {
-                activities = activityRedisTemplateService.findByCapacityGreaterThanEqual(capacity);
-                if(activities.isEmpty()){ // 만약 레디스에 없으면 db호출
-                    activities = activityRepository.findByCapacityGreaterThanEqual(capacity);
-                }
-            } else if (capacity == 0) {
-                activities = activityRedisTemplateService.findByVenueGreaterThanEqual(venue);
-                if(activities.isEmpty()){ // 만약 레디스에 없으면 db호출
-                    activities = activityRepository.findByVenueGreaterThanEqual(capacity);
-                }
-            } else {
-                activities = activityRedisTemplateService.findByVenueAndCapacityGreaterThanEqual(venue, capacity);
-                if(activities.isEmpty()) { // 만약 레디스에 없으면 db호출
-                    activities = activityRepository.findByVenueAndCapacityGreaterThanEqual(venue, capacity);
-                }
+        if (venue == 0 && capacity == 0) {
+            activities = activityRedisTemplateService.findAll(); // Redis에서 모든 활동 객체 목록을 가져옴
+            if (activities.isEmpty()) { // 만약 레디스에 없으면 db호출
+                activities = activityRepository.findAll();
             }
-
-            if (activities.isEmpty()) {
-                return "No activities found.";
+        } else if (venue == 0) {
+            activities = activityRedisTemplateService.findByCapacityGreaterThanEqual(capacity);
+            if (activities.isEmpty()) { // 만약 레디스에 없으면 db호출
+                activities = activityRepository.findByCapacityGreaterThanEqual(capacity);
             }
-
-            Random random = new Random();
-            int index = random.nextInt(activities.size());
-            Activity randomActivity = activities.get(index);
-
-            return randomActivity.getName();
+        } else if (capacity == 0) {
+            activities = activityRedisTemplateService.findByVenueGreaterThanEqual(venue);
+            if (activities.isEmpty()) { // 만약 레디스에 없으면 db호출
+                activities = activityRepository.findByVenueGreaterThanEqual(capacity);
+            }
+        } else {
+            activities = activityRedisTemplateService.findByVenueAndCapacityGreaterThanEqual(venue, capacity);
+            if (activities.isEmpty()) { // 만약 레디스에 없으면 db호출
+                activities = activityRepository.findByVenueAndCapacityGreaterThanEqual(venue, capacity);
+            }
         }
+
+        if (activities.isEmpty()) {
+            return "No activities found.";
+        }
+
+        Random random = new Random();
+        int index = random.nextInt(activities.size());
+        Activity randomActivity = activities.get(index);
+
+        return randomActivity.getName();
+    }
 
 
     public void delete(int id) {
@@ -62,13 +62,47 @@ public class ActivityService {
 
 
     public Activity addActivity(String name, int venue, int capacity) {
-        Activity activity = Activity.builder()
-                .name(name)
-                .venue(venue)
-                .capacity(capacity)
-                .build();
-
-        return activityRepository.save(activity);
+        if (venue == 0 && capacity == 0) {
+            for (int i = 1; i <= 2; i++) {
+                for (int j = 1; j <= 4; j++) {
+                    Activity activity = Activity.builder()
+                            .name(name)
+                            .venue(i)
+                            .capacity(j)
+                            .build();
+                    activityRepository.save(activity);
+                    log.info("addActivity venue{} capacity{}", i, j);
+                }
+            }
+        } else if (venue == 0) {
+            for (int i = 1; i <= 2; i++) {
+                Activity activity = Activity.builder()
+                        .name(name)
+                        .venue(i)
+                        .capacity(capacity)
+                        .build();
+                activityRepository.save(activity);
+                log.info("addActivity venue{}", i);
+            }
+        } else if (capacity == 0) {
+            for (int i = 1; i <= 4; i++) {
+                Activity activity = Activity.builder()
+                        .name(name)
+                        .venue(venue)
+                        .capacity(i)
+                        .build();
+                log.info("addActivity capacity{}", i);
+                activityRepository.save(activity);
+            }
+        } else {
+            Activity activity = Activity.builder()
+                    .name(name)
+                    .venue(venue)
+                    .capacity(capacity)
+                    .build();
+            log.info("addActivity");
+            return activityRepository.save(activity);
+        }
+        return null;
     }
-
-    }
+}
