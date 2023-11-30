4267f272fd2064070a918d4d29bc49ce52e28b24
qwer7824 <sh7824@naver.com>
2023-11-30 20:46:19 +0900

    ActivityController 수정
    페이징 추가 및 Redis 수량만 프론트에 표시하게 변경

--- src/main/java/com/today/demo/controller/activity/ActivityController.java
+++ src/main/java/com/today/demo/controller/activity/ActivityController.java
@@ -1,15 +1,20 @@
 package com.today.demo.controller.activity;
 
 import com.today.demo.entity.Activity;
+import com.today.demo.entity.Marker;
 import com.today.demo.redisService.ActivityRedisTemplateService;
 import com.today.demo.repository.ActivityRepository;
 import com.today.demo.service.ActivityService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -23,17 +28,24 @@ public class ActivityController {
 
     private final ActivityRedisTemplateService activityRedisTemplateService;
     private final ActivityService activityService;
+    private final ActivityRepository activityRepository;
 
     @GetMapping("/admin/activity")
-    public String getAllActivities(Model model) {
+    public String getAllActivities(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            Model model) {
+        Pageable pageable = PageRequest.of(page, size);
         List<Activity> RedisActivities = activityRedisTemplateService.findAll();
-        List<Activity> DBActivities = activityService.allActivity();
+        Page<Activity> activityPage = activityRepository.findAll(pageable);
+        List<Activity> DBActivities = activityPage.getContent();
 
-        RedisActivities.sort(Comparator.comparing(Activity::getId));
-        DBActivities.sort(Comparator.comparing(Activity::getId));
+        int Count = RedisActivities.size();
 
-        model.addAttribute("RedisActivities", RedisActivities);
+        model.addAttribute("RedisCount", Count);
         model.addAttribute("DBActivities", DBActivities);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", activityPage.getTotalPages());
         return "admin/activities";
     }
 }
