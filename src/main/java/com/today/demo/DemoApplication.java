651a3e268abbb117ea0adb9f4187cbcb9e43c054
qwer7824 <sh7824@naver.com>
2024-02-16 03:58:32 +0900

    BaseEntity 추가

--- src/main/java/com/today/demo/DemoApplication.java
+++ src/main/java/com/today/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.today.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
